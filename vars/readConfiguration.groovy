6896bc3ee21f5caf817d7dd2af39c86df4bb669d
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-17 17:00:49 -0500

    Continue...

--- vars/readConfiguration.groovy
+++ vars/readConfiguration.groovy
@@ -2,6 +2,7 @@ def call() {
     echo "Hello"
     echo "${env.APP_WORKDIR}"
     dir(env.APP_WORDIR) {
+        echo "Hello wtf"
         conf = readYml 'continuous-deployment/config.yml'
         return conf
     }
