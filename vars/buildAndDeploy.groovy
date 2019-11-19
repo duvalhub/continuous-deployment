309462311081a454d9838108aedf12136177ed30
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-18 20:30:29 -0500

    Continue...

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -2,7 +2,7 @@
 def call() {
 
   docker.withServer('tcp://docker-dev.philippeduval.ca:2376', env.DOCKER_BUNDLE_ID) {
-      docker.image('git') {
+      docker.image('git').inside() {
           /* do things */
           echo "hello"
           sh "git --version"
