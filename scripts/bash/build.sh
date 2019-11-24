3a103a491125745fe5c063d54598125710398edf
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-24 18:02:01 -0500

    Continue...

--- scripts/bash/build.sh
+++ scripts/bash/build.sh
@@ -5,5 +5,8 @@ mv $DOCKERFILE_PATH Dockerfile
 mv $TEMPLATE_PATH docker
 /usr/bin/docker build -t "$IMAGE" .
 
+echo "hello from script"
+echo $DOCKER_CREDENTIALS_PSW
+echo $DOCKER_CREDENTIALS_USR
 echo "$DOCKER_CREDENTIALS_PSW" | docker login --username "$DOCKER_CREDENTIALS_USR" --password-stdin "$DOCKER_REGISTRY"
 docker push "$FULL_NAME"
