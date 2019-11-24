1b53c50327f7133a50d5d0d9ce4d9f41eee30507
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-24 12:25:27 -0500

    Continue...

--- src/com/duvalhub/AppConfig.groovy
+++ src/com/duvalhub/AppConfig.groovy
@@ -1,7 +1,13 @@
 package com.duvalhub
 
+import groovy.json.JsonBuilder
+
 class AppConfig {
     App app
+
+    String toString() {
+        return new JsonBuilder( this ).toPrettyString()
+    }
 
 }
 
