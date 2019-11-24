13c0ab00f0de1f7853d89bbbcf30cda7cfa04c43
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-24 14:10:09 -0500

    Continue...

--- scripts/bash/build.sh
+++ scripts/bash/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-docker build --build-args TEMPLATE_PATH=$TEMPLATE_PATH -t "$IMAGE" -f $DOCKERFILE_PATH .
+whoami
+/usr/bin/docker build --build-args TEMPLATE_PATH=$TEMPLATE_PATH -t "$IMAGE" -f $DOCKERFILE_PATH .
 
 #echo "$DOCKER_CREDENTIALS_PSW" | docker login --username "$DOCKER_CREDENTIALS_USR" --password-stdin "$DOCKER_REGISTRY"
 #docker push "$FULL_NAME"
