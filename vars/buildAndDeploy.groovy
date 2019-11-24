4d5ae1c097202ed8070bf5a880b92bf0cbe9783e
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-23 22:07:48 -0500

    Continue...

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -1,63 +1,11 @@
 
 def call() {
-
-//node {
-//  echo "hello form container"
-//  sh "git --version"
-//}
-echo "allo"
-echo BRANCH_NAME
-
-dockerSlave() {
-//node {
-  sh "ls -all"
-  checkout scm 
-  echo "hello"
-  sh "ls -all"
-  dir('hello-world') {
-    sh "ls -all"
-  }
-  sh "pwd"
-  sh "git --version"
-  echo "Hello from pipeline"
-
-  sh "env"
-  echo "+++++++++++++++++++++++++++++++++yy"
-//  sh "git remote -v"
-//  echo "+++++++++++++++++++++++++++++++++yy"
-//  def gitUrl = sh returnStdout: true, script: 'git config --get remote.origin.url'
-//  echo gitUrl
-  echo "+++++++++++++++++++++++++++++++++yy"
-  echo "$BRANCH_NAME"
-  echo "+++++++++++++++++++++++++++++++++yy"
-  echo "$GIT_URL"
+  dockerSlave() {
+    checkout scm 
     initializeWorkdirStage()
-
-  return
     conf = readConfiguration()
 
     echo "${conf}"
-}
-/*
-  docker.withServer('tcp://docker-dev.philippeduval.ca:2376', env.DOCKER_BUNDLE_ID) {
-      docker.image('alpine/git').inside() {
-          echo "hello"
-          sh "ls -l"
-          sh "pwd"
-          sh "git --version"
-      }
   }
-  */
-//  node {
-//    echo "Hello from pipeline"
-//
-//    initializeWorkdirStage()
-//
-//    conf = readConfiguration()
-//
-//    echo "${conf}"
-//
-//  }
-
 }
 
