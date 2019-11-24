bba9a3c3be5d978f5ddb673b03a17f918779af89
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-23 21:20:31 -0500

    Continue...

--- vars/initializeWorkdir.groovy
+++ vars/initializeWorkdir.groovy
@@ -1,9 +1,6 @@
 import com.duvalhub.GitCloneRequest
 
 def call() {
-
-    echo "$PWD"
-    echo "$WORKSPACE"
     current = sh returnStdout: true, script: 'pwd'
     echo "${current}"
     GitCloneRequest request = new GitCloneRequest("https://github.com/duvalhub/helloworld-app.git", "hello-world")
