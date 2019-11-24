76f9dcbfec6098d9ea018eb2e0d5c0f134a1ba15
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-24 14:20:34 -0500

    Continue...

--- vars/dockerSlave.groovy
+++ vars/dockerSlave.groovy
@@ -1,6 +1,5 @@
 def call(Closure body) {
-    //def dockerSlaveImage = 'huguesmcd/jenkins-slave:1.0.0'
-    def dockerSlaveImage = 'communitycloud/docker-client'
+    def dockerSlaveImage = 'huguesmcd/jenkins-slave'
     dockerNode(dockerSlaveImage) { 
         body()
     }
