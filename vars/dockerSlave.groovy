e8c4e59b9990e3dad0215932cecd505330229775
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-24 14:46:40 -0500

    Continue...

--- vars/dockerSlave.groovy
+++ vars/dockerSlave.groovy
@@ -2,7 +2,6 @@ def call(Closure body) {
     def dockerSlaveImage = 'huguesmcd/jenkins-slave'
     dockerNode(dockerSlaveImage) { 
         withCredentials([dockerCert(credentialsId: env.DOCKER_BUNDLE_ID, variable: 'DOCKER_CERT_PATH')]) {
-          sh 'use $DOCKER_CERT_PATH'
           body()
         }
     }
