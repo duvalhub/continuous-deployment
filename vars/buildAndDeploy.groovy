eb0c8f7d7f888adbb560a7dfd9a6f79e8a95167f
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-23 21:05:04 -0500

    Continue...

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -1,24 +1,15 @@
 
 def call() {
+  node {
+    echo "Hello from pipeline"
 
-  docker.withServer('tcp://docker-dev.philippeduval.ca:2376', env.DOCKER_BUNDLE_ID) {
-      docker.image('alpine/git').inside() {
-          /* do things */
-          echo "hello"
-          sh "git --version"
-      }
+    initializeWorkdirStage()
+
+    conf = readConfiguration()
+
+    echo "${conf}"
+
   }
-
-//  node {
-//    echo "Hello from pipeline"
-//
-//    initializeWorkdirStage()
-//
-//    conf = readConfiguration()
-//
-//    echo "${conf}"
-//
-//  }
 
 }
 
