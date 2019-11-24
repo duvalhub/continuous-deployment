90bb1dab62856e56f5bc82a892a5a36ceb8b91fe
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-24 12:12:16 -0500

    Continue...

--- src/com/duvalhub/AppConfig.groovy
+++ src/com/duvalhub/AppConfig.groovy
@@ -1,22 +1,12 @@
 package com.duvalhub
 
 class AppConfig {
-    String directory
-    String url
-    String toCheckout
+    App app
 
-    GitCloneRequest(String url, String directory, String toCheckout) {
-        this(url, directory)
-        this.toCheckout = toCheckout
-    }
+}
 
-    GitCloneRequest(String url, String directory) {
-        this(url)
-        this.directory = directory
-    }
-
-    GitCloneRequest(String url ) {
-        this.url = url
-        this.directory = "cloned"
-    }
+class App {
+    String name
+    String type
+    String gros
 }
--- vars/readConfiguration.groovy
+++ vars/readConfiguration.groovy
@@ -1,7 +1,8 @@
+import con.duvalhub.AppConfig
 def call() {
     def workdir = "${env.APP_WORKDIR}"
     dir(workdir) {
-        conf = readYaml file:'continuous-deployment/config.yml'
+        AppConfig conf = readYaml file:'continuous-deployment/config.yml'
         echo "${conf.getClass()}"
         return conf
     }
