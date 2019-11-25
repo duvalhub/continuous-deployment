5b7e016a350a09350265dae50f205e22fb42da65
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-25 09:40:52 -0500

    Continue...

--- src/com/duvalhub/DeployRequest.groovy
+++ src/com/duvalhub/DeployRequest.groovy
@@ -4,12 +4,14 @@ import com.duvalhub.BaseObject
 import com.duvalhub.AppConfig
 
 class DeployRequest extends BaseObject {
+    AppConfig appConfig
     String appName
     String image
 
     DeployRequest(AppConfig appConfig, String version) {
         println "we are hrer"
         this.appName = appConfig.app.name
-        this.image = appConfig.getDockerImage(version)
+        this.appConfig = appConfig
+        //this.image = appConfig.getDockerImage(version)
     }
 }
