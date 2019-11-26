a3c2d49071e9641dd137d32a21be6329f0f3d5ca
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-26 00:25:36 -0500

    Continue...

--- scripts/bash/build.sh
+++ scripts/bash/build.sh
@@ -8,7 +8,8 @@ docker version
 ls -l 
 ls -l docker
 cat docker/nginx.conf
-/usr/bin/docker build --build-arg TEMPLATE_PATH=docker -t "$IMAGE" .
+/usr/bin/docker build TEMPLATE_PATH=docker -t "$IMAGE" .
+#/usr/bin/docker build --build-arg TEMPLATE_PATH=docker -t "$IMAGE" .
 
 echo "$DOCKER_CREDENTIALS_PSW" | docker login --username "$DOCKER_CREDENTIALS_USR" --password-stdin 2> /dev/null
 docker push "$IMAGE"
