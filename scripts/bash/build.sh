08d3db5f5b794efe6ccc9bc098ba54d37bdd0a7a
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-25 23:56:32 -0500

    Continue...

--- scripts/bash/build.sh
+++ scripts/bash/build.sh
@@ -4,6 +4,9 @@ set -e
 mv $DOCKERFILE_PATH Dockerfile
 mv $TEMPLATE_PATH docker
 docker version
+
+ls -l 
+ls -l docker
 /usr/bin/docker build --build-arg TEMPLATE_PATH=docker -t "$IMAGE" .
 
 echo "$DOCKER_CREDENTIALS_PSW" | docker login --username "$DOCKER_CREDENTIALS_USR" --password-stdin 2> /dev/null
