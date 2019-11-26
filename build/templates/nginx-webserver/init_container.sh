db35e9d2a55134fbdce897b55f1eef3c7dfc35e8
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-26 09:30:45 -0500

    Continue...

--- build/templates/nginx-webserver/init_container.sh
+++ build/templates/nginx-webserver/init_container.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ -z "$BACKEND_HOST" ]
-then
-    export BACKEND_HOST=backend
-fi
-
-envsubst '\$BACKEND_HOST' < "$1" > "$1"
+#if [ -z "$BACKEND_HOST" ]
+#then
+#    export BACKEND_HOST=backend
+#fi
+#
+#envsubst '\$BACKEND_HOST' < "$1" > "$1"
 
 echo "### Nginx configuration"
 cat $1
