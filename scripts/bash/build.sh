89fbd02b8c6346882ad49c67a9f3efd7b96dbf94
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-11-26 00:05:06 -0500

    Continue...

--- scripts/bash/build.sh
+++ scripts/bash/build.sh
@@ -7,6 +7,7 @@ docker version
 
 ls -l 
 ls -l docker
+cat docker/nginx.conf
 /usr/bin/docker build --build-arg TEMPLATE_PATH=docker -t "$IMAGE" .
 
 echo "$DOCKER_CREDENTIALS_PSW" | docker login --username "$DOCKER_CREDENTIALS_USR" --password-stdin 2> /dev/null
