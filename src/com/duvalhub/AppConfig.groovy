df077111b90fd68a5dca6a87feae8c67d907c04f
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-12-03 23:55:09 -0500

    Continue...

--- src/com/duvalhub/AppConfig.groovy
+++ src/com/duvalhub/AppConfig.groovy
@@ -6,6 +6,7 @@ import groovy.json.JsonBuilder
 class AppConfig extends BaseObject {
     App app
     Docker docker
+    Epais unpais
 
     String getDockerImage(){
         return "${this.docker.registry}/${this.docker.namespace}/${this.docker.repository}"
@@ -14,7 +15,9 @@ class AppConfig extends BaseObject {
         return "${this.getDockerImage()}:${version}"
     }
 }
-
+class Epais {
+    String unpais
+}
 class App {
     String name
     String type
