02bf453cd3d7e0fa7e43de9ebc17d044491f9a43
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-12-05 01:07:35 -0500

    Continue...

--- src/com/duvalhub/WriteComposeRequest.groovy
+++ src/com/duvalhub/WriteComposeRequest.groovy
@@ -28,9 +28,10 @@ class WriteComposeRequest extends BaseObject {
     String getHosts() {
         String urls = "${this.appName}.${this.config.app.group}.${this.request.environment}.${this.base}"
         
-        //urls = "${urls},${this.hosts}"
 
-//        if(this.hosts) {}
+        if(this.hosts) {
+            urls = urls + this.hosts
+        }
 
         println("#######################################")
         println(urls)
