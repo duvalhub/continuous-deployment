25c7850e2b11421637cfacf2fa46eb38d8985712
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-12-08 22:23:34 -0500

    Continue...

--- vars/writeCompose.groovy
+++ vars/writeCompose.groovy
@@ -5,6 +5,7 @@ def call(WriteComposeRequest request) {
   env.APP_NAME = request.appName
   env.IMAGE = request.getImage()
   env.HOSTS = request.getDomainNames()
+  echo request.toString()
   env.PORT = request.port
   def processScript = "${env.PIPELINE_WORKDIR}/${request.scriptPath}"
   def compose = request.compose
