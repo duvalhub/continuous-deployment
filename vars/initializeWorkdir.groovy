d51f47c8da27fd3e5997e9305ed56df938c3f905
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-12-16 15:21:20 -0500

    Continue...

--- vars/initializeWorkdir.groovy
+++ vars/initializeWorkdir.groovy
@@ -14,7 +14,7 @@ def call(InitializeWorkdirIn params = new InitializeWorkdirIn()) {
 
     echo "org: '${org}', repo: '${repo}'"
     def configUrl = String.format("https://raw.githubusercontent.com/duvalhub/continous-deployment-configs/master/%s/%s/config.yml", org, repo)
-    def response = httpRequest(configUrl, outputFile: "config.yml")
+    def response = httpRequest(configUrl, {outputFile: "config.yml"})
 
     sh "ls -l"
     sh "cat config.yml"
