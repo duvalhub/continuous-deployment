a07aad75eb84baae9a46519b405e91b264cf580c
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-01-30 23:03:57 -0500

    Continue...

--- vars/setDockerEnvironment.groovy
+++ vars/setDockerEnvironment.groovy
@@ -1,11 +1,11 @@
-def call(DeployRequest deployRequest, Closure body) {
+def call(DockerHost dockerHost, Closure body) {
     withCredentials([
-        dockerCert(credentialsId: deployRequest.getBundleId(), variable: 'DOCKER_CERT_PATH'),
-        usernamePassword(credentialsId: deployRequest.getCredentialId(), usernameVariable: 'DOCKER_CREDENTIALS_USR', passwordVariable: 'DOCKER_CREDENTIALS_PSW')
+        dockerCert(credentialsId: dockerHost.bundleId, variable: 'DOCKER_CERT_PATH'),
+        usernamePassword(credentialsId: dockerHost.credentialId, usernameVariable: 'DOCKER_CREDENTIALS_USR', passwordVariable: 'DOCKER_CREDENTIALS_PSW')
     ]) {
       echo "${env.DOCKER_CREDENTIALS_USR}"
       echo "${env.DOCKER_CREDENTIALS_PSW}"
-      env.DOCKER_HOST = deployRequest.getDockerUrl()
+      env.DOCKER_HOST = dockerHost.getUrl()()
       env.DOCKER_TLS_VERIFY = 1
       body()
     }
