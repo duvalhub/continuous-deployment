cdf7a18bcfb75002ca2b7b58dd3a985bf19b7d07
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-06 09:45:39 -0500

    Continue...

--- vars/setDockerEnvironment.groovy
+++ vars/setDockerEnvironment.groovy
@@ -3,8 +3,7 @@ import com.duvalhub.appconfig.DockerHost
 def call(DockerHost dockerHost, Closure body) {
   echo "Setting docker environment. dockerHost: '${dockerHost.toString()}'"
   withCredentials([
-      dockerCert(credentialsId: dockerHost.bundleId, variable: 'DOCKER_CERT_PATH'),
-
+      dockerCert(credentialsId: dockerHost.bundleId, variable: 'DOCKER_CERT_PATH')
   ]) {
     env.DOCKER_HOST = dockerHost.getDockerUrl()
     env.DOCKER_TLS_VERIFY = 1
@@ -14,7 +13,7 @@ def call(DockerHost dockerHost, Closure body) {
 
 def withCredentials(DockerHost dockerHost, String credentialId, Closure body) {
   withCredentials([
-    usernamePassword(credentialsId: dockerHost.credentialId, usernameVariable: 'DOCKER_CREDENTIALS_USR', passwordVariable: 'DOCKER_CREDENTIALS_PSW')
+    usernamePassword(credentialsId: credentialId, usernameVariable: 'DOCKER_CREDENTIALS_USR', passwordVariable: 'DOCKER_CREDENTIALS_PSW')
   ]) {
     setDockerEnvironment(dockerHost, body)
   }
