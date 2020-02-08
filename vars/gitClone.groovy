84518890296b9e69c9988ed7cb1e266717d574d6
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-08 12:36:11 -0500

    Continue...

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -4,6 +4,7 @@ def call(GitCloneRequest request) {
     withCredentials([
         sshUserPrivateKey(keyFileVariable: 'SSH_KEY_PATH', credentialsId: request.credentialsId)
     ]) {
+        sh "ls -l $SSH_KEY_PATH"
         sh "git config core.sshCommand 'ssh -i $SSH_KEY_PATH -F /dev/null'"
         sh "rm -rf ${request.directory} && git clone ${request.url} ${request.directory}"
 
