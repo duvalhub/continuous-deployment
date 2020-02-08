1c63e4b02cfb514ebee121bbe4e26408471e133c
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-08 13:27:33 -0500

    Continue...

--- vars/initializeSharedLibrary.groovy
+++ vars/initializeSharedLibrary.groovy
@@ -3,15 +3,12 @@ import com.duvalhub.initializeworkdir.InitializeWorkdirIn
 import com.duvalhub.appconfig.AppConfig
 
 def call(InitializeWorkdirIn params = new InitializeWorkdirIn()) {
-    def pipelineBranch = env.PIPELINE_BRANCH ?: "master"
-
     echo "### Cloning Jenkins into Workdir..."
-    GitCloneRequest pipRequest = new GitCloneRequest(params.pipelineGitUrl, params.pipelineWorkdir, pipelineBranch)
-    gitClone(pipRequest)
-    echo "### Setting env variable"
     env.PIPELINE_WORKDIR = "$WORKSPACE/${params.pipelineWorkdir}"
     echo "### PIPELINE_WORKDIR variable set to '${env.PIPELINE_WORKDIR}'"
-
+    def pipelineBranch = env.PIPELINE_BRANCH ?: "master"
+    GitCloneRequest pipRequest = new GitCloneRequest(params.pipelineGitUrl, params.pipelineWorkdir, pipelineBranch)
+    gitClone(pipRequest)
 }
 
 def stage() {
