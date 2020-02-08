068b98a4ebaffd6fad223d4355b5a3e186893b39
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-08 13:45:12 -0500

    Continue...

--- scripts/bash/gitclone/gitclone.sh
+++ scripts/bash/gitclone/gitclone.sh
@@ -3,5 +3,5 @@
 echo "### GitClone script"
 echo "### GIT_URL: '$GIT_URL', GIT_DIRECTORY: '$GIT_DIRECTORY', SSH_KEY_PATH: '$SSH_KEY_PATH'"
 rm -rf $GIT_DIRECTORY
-export GIT_SSH_COMMAND="ssh -i $SSH_KEY_PATH  -F /dev/null" 
-git clone $GIT_URL $GIT_DIRECTORY
+#export GIT_SSH_COMMAND="ssh -i $SSH_KEY_PATH  -F /dev/null" 
+GIT_SSH_COMMAND="ssh -i $SSH_KEY_PATH  -F /dev/null" git clone $GIT_URL $GIT_DIRECTORY
