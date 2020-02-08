3bae5a711875b941c4efb05e3c2ba46fe82472de
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-08 12:43:53 -0500

    Continue...

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -4,13 +4,15 @@ def call(GitCloneRequest request) {
     withCredentials([
         sshUserPrivateKey(keyFileVariable: 'SSH_KEY_PATH', credentialsId: request.credentialsId)
     ]) {
-        sh "ls -l $SSH_KEY_PATH"
-        sh "git config core.sshCommand 'ssh -i $SSH_KEY_PATH -F /dev/null'"
-        sh "rm -rf ${request.directory} && git clone ${request.url} ${request.directory}"
+        withEnv([
+            "GIT_SSH_COMMAND='ssh -i ${env.SSH_KEY_PATH} -F /dev/null'"
+        ]) {
+            sh "rm -rf ${request.directory} && git clone ${request.url} ${request.directory}"
 
-        if ( request.toCheckout ) {
-            dir( request.directory) {
-                sh "git checkout ${request.toCheckout}"
+            if ( request.toCheckout ) {
+                dir( request.directory) {
+                    sh "git checkout ${request.toCheckout}"
+                }
             }
         }
     }
