ac25065f8bf6888b6f6492b5914315354caf6b95
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-08 13:41:20 -0500

    Continue...

--- vars/initializeSharedLibrary.groovy
+++ vars/initializeSharedLibrary.groovy
@@ -7,8 +7,8 @@ def call(InitializeWorkdirIn params = new InitializeWorkdirIn()) {
     env.PIPELINE_WORKDIR = "$WORKSPACE/${params.pipelineWorkdir}"
     echo "### PIPELINE_WORKDIR variable set to '${env.PIPELINE_WORKDIR}'"
     def pipelineBranch = env.PIPELINE_BRANCH ?: "master"
-
-    sh "git clone ${params.pipelineGitUrl} -b ${pipelineBranch} ${params.pipelineWorkdir}"
+    sh "find $PIPELINE_WORKDIR"
+    sh "rm -rf $PIPELINE_WORKDIR && git clone ${params.pipelineGitUrl} -b ${pipelineBranch} $PIPELINE_WORKDIR"
 //    GitCloneRequest pipRequest = new GitCloneRequest(params.pipelineGitUrl, params.pipelineWorkdir, pipelineBranch)
 //    gitClone(pipRequest)
 }
