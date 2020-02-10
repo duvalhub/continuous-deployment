f6a36b7ee223764273ccce17028890e3913702cd
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-09 23:56:08 -0500

    Continue...

--- src/com/duvalhub/git/GitCloneRequest.groovy
+++ src/com/duvalhub/git/GitCloneRequest.groovy
@@ -1,25 +1,20 @@
 package com.duvalhub.git
 
 import com.duvalhub.BaseObject
+import com.duvalhub.git.GitRepo
 
 class GitCloneRequest extends BaseObject {
     String directory
-    String url
-    String toCheckout
+    GitRepo gitRepo
     String credentialsId = "SERVICE_ACCOUNT_SSH"
 
-    GitCloneRequest(String url, String directory, String toCheckout) {
-        this(url, directory)
+    GitCloneRequest(GitRepo gitRepo, String toCheckout) {
+        this(gitRepo)
         this.toCheckout = toCheckout
     }
 
-    GitCloneRequest(String url, String directory) {
-        this(url)
-        this.directory = directory
-    }
-
-    GitCloneRequest(String url ) {
-        this.url = url
+    GitCloneRequest(GitRepo gitRepo ) {
+        this.gitRepo = gitRepo
         this.directory = "cloned"
     }
 }
