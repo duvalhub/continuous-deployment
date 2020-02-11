6a0ea303e92de6535aca0be261974c94322b168a
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-10 22:17:15 -0500

    Continue...

--- vars/executeScript.groovy
+++ vars/executeScript.groovy
@@ -1,3 +1,3 @@
 def call(String script_path, boolean stdout = false) {
-    return sh(returnStdout: stdout, script: "chmod +x ${script_path} && ${script_path}").trim()
+    return sh(returnStdout: stdout, script: "chmod +x ${script_path} && ${script_path}")
 }
