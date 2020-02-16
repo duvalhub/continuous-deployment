2f9a030fe56e951dda364fbcfb41e86d5a665e00
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-15 19:45:12 -0500

    Continue...

--- scripts/bash/getReleaseBranch/getReleaseBranch.sh
+++ scripts/bash/getReleaseBranch/getReleaseBranch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 git fetch
-version=$(git branch -a | grep release | awk -F"release/" '{print $2}')
-echo -n "release/$version"
+release_branch_count=$(git branch -a | grep release | wc -l); 
+
+if (( $release_branch_count > 0 )); then;
+    version=$(git branch -a | grep release | awk -F"release/" '{print $2}')
+    echo -n "release/$version"
+else 
+    >&2 echo "There is not release branch. Failing" 
+    exit 1
+fi
