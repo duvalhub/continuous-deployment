536c9f793d6a7feb6cacd72cf13c5035744a1a55
Maxthod <47720660+Maxthod@users.noreply.github.com>
2020-07-01 17:07:49 -0400

    Fix/broken checkout scm (#10)
    Missing ssh key during git commands

--- vars/processBranchName.groovy
+++ vars/processBranchName.groovy
@@ -1,58 +1,61 @@
 import com.duvalhub.processbranchname.ProcessBranchNameRequest
 import com.duvalhub.processbranchname.ProcessBranchNameResponse
-import com.duvalhub.appconfig.AppConfig
 
 def call(ProcessBranchNameRequest request) {
-  ProcessBranchNameResponse response = new ProcessBranchNameResponse()
-  String branchName = request.branchName
-  def releasePattern = /release\/(.*)/
+    ProcessBranchNameResponse response = new ProcessBranchNameResponse()
+    String branchName = request.branchName
+    def releasePattern = /release\/(.*)/
 
-  switch(branchName) {
-      case "develop":
-      case ~/test.*/:
-          response.doBuild = true
-          response.version = "latest"
-          response.doDeploy = true
-          response.deployEnv = "dev"
-          break
-  
-      case ~releasePattern:
-          def matcher = branchName =~ releasePattern
-          def version = matcher[0][1]
-          response.doBuild = true
-          response.version = version
-          response.doDeploy = true
-          response.deployEnv = "stage"
-          break
-  
-      case "master":
-          response.doDeploy = true
-          dir( env.APP_WORKDIR ) {
-            sh "git fetch --tags > /dev/null"
-            response.version = sh (returnStdout: true, script: '''
-                echo $(git tag --points-at HEAD)
+    switch (branchName) {
+        case "develop":
+        case ~/test.*/:
+            response.doBuild = true
+            response.version = "latest"
+            response.doDeploy = true
+            response.deployEnv = "dev"
+            break
+
+        case ~releasePattern:
+            def matcher = branchName =~ releasePattern
+            def version = matcher[0][1]
+            response.doBuild = true
+            response.version = version
+            response.doDeploy = true
+            response.deployEnv = "stage"
+            break
+
+        case "master":
+            response.doDeploy = true
+            dir(env.APP_WORKDIR) {
+                withSshKey() {
+                    sh "git remote -v"
+                    sh "git pull"
+                    sh "git fetch --tags > /dev/null"
+                    response.version = sh(returnStdout: true, script: '''
+                    echo $(git tag --points-at HEAD)
                 ''').trim()
-          }
-          response.deployEnv = "prod"
-          break
-      default:
-        response.doBuild = true
-        response.version = "latest"
-        response.doDeploy = false
-  }
+                }
+            }
+            response.deployEnv = "prod"
+            break
+        default:
+            response.doBuild = true
+            response.version = "latest"
+            response.doDeploy = false
+    }
 
-  if ( response.doBuild ) {
-      echo "Building version '${response.version}' from branch '${branchName}'"
-  } else {
-      echo "We don't build this branch: '${branchName}'"
-  }
- 
-  if ( response.doDeploy ) {
-      echo "Promoting version '${response.version}' in '${response.deployEnv}'"
-  } else {
-      echo "We don't deploy this branch: '${branchName}'"
-  }
-  
-  return response
+    if (response.doBuild) {
+        echo "Building version '${response.version}' from branch '${branchName}'"
+    } else {
+        echo "We don't build this branch: '${branchName}'"
+    }
+
+    if (response.doDeploy) {
+        echo "Promoting version '${response.version}' in '${response.deployEnv}'"
+    } else {
+        echo "We don't deploy this branch: '${branchName}'"
+    }
+
+    return response
 }
 
