4e7e205c64938d4fe479b72cb3d90b9911933ab4
Hugues McNeil-Duval <hugues.mcneil-duval@duvalhub.com>
2022-12-09 23:50:29 -0500

    Continue...

--- vars/processBranchName.groovy
+++ vars/processBranchName.groovy
@@ -86,7 +86,7 @@ def setAsOneBranch(ProcessBranchNameRequest request, ProcessBranchNameResponse r
             break
         default:
             response.doBuild = true
-            response.version = String.format("%s-%s", branchName, version)
+            response.version = String.format("%s-%s", branchName.replaceAll("/", "-"), version)
             response.doDeploy = true
             response.deployEnv = "dev"
             break
